3366c4c80ec6b110269504d9a5ba7d6560d98755
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-18 03:31:30 +0900

    feat: `reset`, `global` 전역 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./router/Router";
+
+import "./styles/reset.css";
+import "./styles/global.css";
+
+import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

6661bd72af6acb41f09f24180260bb9811bb0cb1
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-18 04:23:55 +0900

    refactor: h2 -> p 로 수정

--- src/components/NavigationBar/Category/index.tsx
+++ src/components/NavigationBar/Category/index.tsx
@@ -11,7 +11,7 @@ interface Props extends PropsWithChildren {
 export default function Category({ name, children }: Props) {
   return (
     <Container>
-      <h2>{name}</h2>
+      <p>{name}</p>
       <Container>{children}</Container>
     </Container>
   );

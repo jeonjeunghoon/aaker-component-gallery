942d4b616b10dfee16ca5c1ce0750069fa5e8779
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-18 06:01:04 +0900

    chore: `gh-page` 세팅

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import { BASE_URL } from "../constants/url";
+
 import App from "../App";
 import ContainerPage from "../pages/ContainerPage";
 import GridPage from "../pages/GridPage";
@@ -8,7 +10,7 @@ import FlexPage from "../pages/FlexPage";
 export default function Router() {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: BASE_URL,
       element: <App />,
       children: [
         {
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,12 @@
-import { useNavigate } from "react-router-dom";
-
 import styles from "./index.module.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  const navigate = useNavigate();
-
   return (
     <header className={styles["header"]}>
-      <button onClick={() => navigate("/")}>
+      <Link to=''>
         <h1 className={styles["title"]}>☕️ Aaker's component gallery</h1>
-      </button>
+      </Link>
     </header>
   );
 }
